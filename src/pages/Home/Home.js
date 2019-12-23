ff6cadbddd8e1a080e3809af1dce12053c8c625b
ivanfslee <ivanfslee@gmail.com>
2019-12-23 15:40:29 -0800

    Used promise.all to make multiple api requests to get multiple cities and render in a carousel

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,17 +7,57 @@ import Cities from '../../utility/City/Cities';
 
 class Home extends Component {
     state = {
-        cities: []
+        cities: [],
+        europeCities: {},
+        asiaCities: {},
+        exoticCities: {}
     }
 
     async componentDidMount() {
-        const recommendedCities = await axios.get(`${window.apiHost}/cities/recommended`);
-        // console.log(recommendedCities.data);
-        this.setState({cities: recommendedCities.data});
+        const recommendedCities = axios.get(`${window.apiHost}/cities/recommended`);
+        const europeCitiesUrl = axios.get(`${window.apiHost}/cities/europe`);
+        const asiaCitiesUrl = axios.get(`${window.apiHost}/cities/asia`);
+        const exoticCitiesUrl = axios.get(`${window.apiHost}/cities/exotic`);
+
+        const citiesPromises = [recommendedCities, europeCitiesUrl, asiaCitiesUrl, exoticCitiesUrl];
+
+        Promise.all(citiesPromises).then(data => {
+            const recommendedCities = (data[0].data);
+            const europeCities = (data[1].data);
+            const asiaCities = (data[2].data);
+            const exoticCities = (data[3].data);
+            this.setState({
+                cities: recommendedCities,
+                europeCities,
+                asiaCities,
+                exoticCities
+            });
+        })
+
+        // const recommendedCities = `${window.apiHost}/cities/recommended`;
+        // const europeCitiesUrl = `${window.apiHost}/cities/europe`;
+        // const asiaCitiesUrl = `${window.apiHost}/cities/asia`;
+        // const exoticCitiesUrl = `${window.apiHost}/cities/exotic`;
+
+        // const citiesPromises = [];
+
+        // citiesPromises.push(axios.get(recommendedCities));
+        // citiesPromises.push(axios.get(europeCitiesUrl));
+        // citiesPromises.push(axios.get(asiaCitiesUrl));
+        // citiesPromises.push(axios.get(exoticCitiesUrl));
+
+        // Promise.all(citiesPromises).then(data => {
+        //     console.log(data[0].data);
+        //     console.log(data[1].data);
+        //     console.log(data[2].data);
+        //     console.log(data[3].data);
+        // })
+
+
     }
 
     render() {
-        // console.log(this.state.cities);
+        console.log(this.state.cities);
         if (this.state.cities.length === 0) {
             return (
                 <Spinner />
@@ -25,6 +65,7 @@ class Home extends Component {
         }
 
         return (
+            <>
             <div className="container-fluid">
                 <div className="row">
                     <div className="home col s12">
@@ -32,12 +73,26 @@ class Home extends Component {
                             <SearchBox />
                         </div>
                     </div>
-                    <div className="col s12">
-                        <Cities cities={this.state.cities} />    
-                    </div>
-                
                 </div>
             </div>
+            <div className="container-fluid lower-fold">
+                <div className="row">
+                    <div className="col s12">
+                        <Cities cities={this.state.cities} header="Recommended Cities For You" />    
+                    </div>
+                    <div className="col s12">
+                        <Cities cities={this.state.europeCities.cities} header={this.state.europeCities.header} />    
+                    </div>
+                    <div className="col s12">
+                        <Cities cities={this.state.asiaCities.cities} header={this.state.asiaCities.header} />    
+                    </div>
+                    <div className="col s12">
+                        <Cities cities={this.state.exoticCities.cities} header={this.state.exoticCities.header} />    
+                    </div>
+
+                </div>
+            </div>
+            </>
         );
     }
 }
