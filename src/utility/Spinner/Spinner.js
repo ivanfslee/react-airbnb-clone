20d9175d235a04404c10b3ef5e959dc1845c179a
ivanfslee <ivanfslee@gmail.com>
2019-12-22 21:37:11 -0800

    Finished adding spinner component. Will show on load up of app

--- src/utility/Spinner/Spinner.js
+++ src/utility/Spinner/Spinner.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './Spinner.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+library.add(faSpinner); //add spinner icon to the library
 
 class Spinner extends Component {
     render() {
         return (
-            <div className="spinner">
-                <h1>Sanity Check Spinner</h1>
+            <div className="spinner-wrapper">
+                <FontAwesomeIcon icon="spinner" size="6x" spin />
             </div>
         );
     }
