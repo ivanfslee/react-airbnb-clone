56f126f49663fd4aa5e9e57ce51a4766be47bb7b
ivanfslee <ivanfslee@gmail.com>
2019-12-22 21:31:07 -0800

    Added slider, spinner, city components

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import './Home.css';
 import SearchBox from './SearchBox';
+import axios from 'axios';
+import Spinner from '../../utility/Spinner/Spinner';
 
 class Home extends Component {
+    state = {
+        cities: []
+    }
+
+    async componentDidMount() {
+        const recommendedCities = await axios.get(`${window.apiHost}/cities/recommended`);
+        // console.log(recommendedCities.data);
+        this.setState({cities: recommendedCities.data});
+    }
+
     render() {
+        // console.log(this.state.cities);
+        if (this.state.cities.length === 0) {
+            return (
+                <Spinner />
+            );
+        }
+        
         return (
             <div className="container-fluid">
                 <div className="row">
---
+++ src/utility/Spinner/Spinner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Spinner.css';
+
+class Spinner extends Component {
+    render() {
+        return (
+            <div className="spinner">
+                <h1>Sanity Check Spinner</h1>
+            </div>
+        );
+    }
+}
+
+export default Spinner;
